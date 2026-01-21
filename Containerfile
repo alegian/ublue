faa395e372ae5d73df4c9a5beb3a0a466a4c404e
alegian <alegiannx@gmail.com>
2026-01-21 16:15:30 +0200

    fix broken install

--- Containerfile
+++ Containerfile
@@ -25,6 +25,4 @@ RUN --mount=type=bind,from=ctx,source=/,target=/ctx \
     --mount=type=tmpfs,dst=/tmp \
     /ctx/build.sh
     
-### LINTING
-## Verify final image and contents are correct.
 RUN bootc container lint
--- build_files/build.sh
+++ build_files/build.sh
@@ -2,15 +2,7 @@
 
 set -ouex pipefail
 
-### Install packages
-
-# Packages can be installed from any enabled yum repo on the image.
-# RPMfusion repos are available by default in ublue main images
-# List of rpmfusion packages can be found here:
-# https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/43/x86_64/repoview/index.html&protocol=https&redirect=1
-
-# this installs a package from fedora repos
-dnf5 install -y tmux 
+dnf install -y tmux 
 
 # Use a COPR Example:
 #

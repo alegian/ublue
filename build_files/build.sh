e2affb5be1343fd3151fdaaa3019d04059758309
alegian <alegiannx@gmail.com>
2026-01-22 23:44:28 +0200

    modern gtk settings

--- build_files/build.sh
+++ build_files/build.sh
@@ -4,7 +4,7 @@ set -ouex pipefail
 
 dnf5 install -y tmux sway-config-fedora NetworkManager-openvpn \
   nm-connection-editor network-manager-applet pavucontrol dolphin \
-  blueman-applet btop ddcutil light qt6ct lxappearance alacritty
+  blueman-applet btop ddcutil light qt6ct nwg-look alacritty
 
 # Use a COPR Example:
 #

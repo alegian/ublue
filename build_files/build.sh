610729da1a452de7edec02383fa01682d769d85e
alegian <alegiannx@gmail.com>
2026-01-22 13:31:37 +0200

    some basic packages

--- build_files/build.sh
+++ build_files/build.sh
@@ -2,7 +2,7 @@
 
 set -ouex pipefail
 
-dnf5 install -y tmux sway-config-fedora
+dnf5 install -y tmux sway-config-fedora networkmanager-openvpn nm-connection-editor network-manager-applet pavucontrol
 
 # Use a COPR Example:
 #

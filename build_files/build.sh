8e2b03f038a1eac3b1a5b426586b3ba157a0b895
alegian <alegiannx@gmail.com>
2026-01-23 00:09:51 +0200

    possible fix

--- build_files/build.sh
+++ build_files/build.sh
@@ -2,10 +2,12 @@
 
 set -ouex pipefail
 
-sudo dnf copr enable tofik/nwg-shell
+sudo dnf copr enable -y tofik/nwg-shell
 
-dnf5 install -y tmux sway-config-fedora NetworkManager-openvpn \
+dnf5 install -y sway-config-fedora NetworkManager-openvpn \
   nm-connection-editor network-manager-applet pavucontrol dolphin \
   blueman-applet btop ddcutil light qt6ct nwg-look alacritty
+
+sudo dnf copr disable -y tofik/nwg-shell
 
 systemctl enable podman.socket

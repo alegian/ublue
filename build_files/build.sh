8a695c81517cffe62a1797414fef6aaa3f3cd95d
alegian <alegiannx@gmail.com>
2026-01-24 23:45:38 +0200

    :)

--- build_files/build.sh
+++ build_files/build.sh
@@ -9,7 +9,7 @@ sudo dnf5 copr enable -y erikreider/SwayNotificationCenter
 dnf5 install -y sway-config-fedora swayfx NetworkManager-openvpn \
   nm-connection-editor network-manager-applet pavucontrol wlogout \
   blueman-applet btop ddcutil light qt6ct nwg-look alacritty ripgrep \
-  gammastep gvfs nautilus SwayNotificationCenter chezmoi make
+  gammastep gvfs nautilus SwayNotificationCenter chezmoi make gcc
 
 sudo dnf5 copr disable -y tofik/nwg-shell
 sudo dnf5 copr disable -y swayfx/swayfx

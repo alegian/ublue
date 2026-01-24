aefee53d30c202b853c77baa8988536ad5397a5b
alegian <alegiannx@gmail.com>
2026-01-24 15:01:59 +0200

    better iso script

--- iso.sh
+++ iso.sh
@@ -1,1 +1,6 @@
-just clean && just _build-bib ghcr.io/alegian/ublue latest anaconda-iso disk_config/iso.toml
+#!/bin/bash
+
+podman pull ghcr.io/alegian/ublue:latest
+
+just clean
+just _build-bib ghcr.io/alegian/ublue latest anaconda-iso disk_config/iso.toml

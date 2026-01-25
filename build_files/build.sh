b05b09e1058b4875137f5cd21808f1d48dd55cd9
alegian <alegiannx@gmail.com>
2026-01-25 21:12:26 +0200

    cliphist

--- build_files/build.sh
+++ build_files/build.sh
@@ -5,19 +5,18 @@ set -ouex pipefail
 sudo dnf5 copr enable -y tofik/nwg-shell
 sudo dnf5 copr enable -y swayfx/swayfx
 sudo dnf5 copr enable -y erikreider/SwayNotificationCenter
+sudo dnf5 copr enable -y alternateved/cliphist
 
 dnf5 install -y sway-config-fedora swayfx NetworkManager-openvpn \
   nm-connection-editor network-manager-applet pavucontrol wlogout \
   blueman-applet btop ddcutil light qt6ct nwg-look alacritty ripgrep \
-  gammastep gvfs nautilus SwayNotificationCenter chezmoi make gcc \
-  golang
+  gammastep gvfs nautilus SwayNotificationCenter chezmoi make gcc cliphist 
 
-sudo dnf5 copr disable -y tofik/nwg-shell
-sudo dnf5 copr disable -y swayfx/swayfx
+sudo dnf5 copr disable -y alternateved/cliphist
 sudo dnf5 copr disable -y erikreider/SwayNotificationCenter
+sudo dnf5 copr disable -y swayfx/swayfx
+sudo dnf5 copr disable -y tofik/nwg-shell
 
 bash "$(dirname "$0")/neovim.sh"
-
-GOBIN=/usr/bin go install go.senan.xyz/cliphist@latest
 
 systemctl enable podman.socket

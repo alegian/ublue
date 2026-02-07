203501004c81dc663521bcad6f93edcb3ae81761
alegian <alegiannx@gmail.com>
2026-02-07 22:11:15 +0200

    another way

--- build_files/build.sh
+++ build_files/build.sh
@@ -23,11 +23,11 @@ sudo dnf5 copr disable -y swayfx/swayfx
 sudo dnf5 copr disable -y tofik/nwg-shell
 
 sudo dnf install -y https://api2.cursor.sh/updates/download/golden/linux-x64-rpm/cursor/2.4
+sudo dnf install -y https://rpmfind.net/linux/mageia/distrib/cauldron/x86_64/media/core/release/aseprite-1.3.9.1-3.mga10.x86_64.rpm
 
 cp "$(dirname "$0")/setup.sh" /etc/profile.d/setup.sh
 
 bash "$(dirname "$0")/neovim.sh"
-bash "$(dirname "$0")/aseprite.sh"
 
 export PROTON_PASS_CLI_INSTALL_DIR=/usr/bin
 curl -fsSL https://proton.me/download/pass-cli/install.sh | bash

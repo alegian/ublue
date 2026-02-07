74d25790f53274668d20d252a852473ba55588b6
alegian <alegiannx@gmail.com>
2026-02-07 21:31:21 +0200

    try auto mode

--- build_files/aseprite.sh
+++ build_files/aseprite.sh
@@ -4,23 +4,10 @@ sudo dnf5 install -y gcc-c++ clang libcxx-devel cmake ninja-build libX11-devel l
 
 git clone --recursive https://github.com/aseprite/aseprite.git
 cd aseprite
-mkdir build
-cd build
-export CC=clang
-export CXX=clang++
-cmake \
-  -DCMAKE_BUILD_TYPE=RelWithDebInfo \
-  -DCMAKE_CXX_FLAGS:STRING=-stdlib=libstdc++ \
-  -DCMAKE_EXE_LINKER_FLAGS:STRING=-stdlib=libstdc++ \
-  -DLAF_BACKEND=skia \
-  -DSKIA_DIR=$HOME/deps/skia \
-  -DSKIA_LIBRARY_DIR=$HOME/deps/skia/out/Release-x64 \
-  -DSKIA_LIBRARY=$HOME/deps/skia/out/Release-x64/libskia.a \
-  -G Ninja \
-  ..
-ninja aseprite
 
-cp bin/aseprite /usr/bin/aseprite
+bash build.sh --auto
 
-cd ../..
+cp build/bin/aseprite /usr/bin/aseprite
+
+cd ..
 rm -rf aseprite

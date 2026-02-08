d24462a8668befd7a77d1f8d59a87a89c5b73a62
alegian <alegiannx@gmail.com>
2026-02-08 14:44:47 +0200

    ghcup

--- build_files/build.sh
+++ build_files/build.sh
@@ -28,6 +28,7 @@ cp "$(dirname "$0")/setup.sh" /etc/profile.d/setup.sh
 
 bash "$(dirname "$0")/neovim.sh"
 bash "$(dirname "$0")/aseprite.sh"
+bash "$(dirname "$0")/ghcup.sh"
 
 export PROTON_PASS_CLI_INSTALL_DIR=/usr/bin
 curl -fsSL https://proton.me/download/pass-cli/install.sh | bash
---
+++ build_files/ghcup.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+sudo dn5 install -y gcc gcc-c++ gmp gmp-devel make ncurses ncurses-compat-libs xz perl

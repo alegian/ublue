9d3c45c607c6109ec9fb037d78be5a43f13e92b2
alegian <alegiannx@gmail.com>
2026-02-11 09:44:14 +0200

    dont even install

--- build_files/build.sh
+++ build_files/build.sh
@@ -14,8 +14,7 @@ dnf5 install -y sway-config-fedora swayfx NetworkManager-openvpn golang \
   nm-connection-editor network-manager-applet pavucontrol wlogout \
   blueman-applet btop ddcutil light qt6ct nwg-look alacritty ripgrep \
   gammastep gvfs nautilus SwayNotificationCenter chezmoi make gcc cliphist \
-  java-25-openjdk maven grim slurp yazi age gtklock ImageMagick greetd \
-  gtkgreet
+  java-25-openjdk maven grim slurp yazi age gtklock ImageMagick 
 
 sudo dnf5 copr disable -y lihaohong/yazi
 sudo dnf5 copr disable -y alternateved/cliphist

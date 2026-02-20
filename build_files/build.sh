9d34a3e2bec8b46c4b38f8ee18854aa53e26d1cf
alegian <alegiannx@gmail.com>
2026-02-20 11:10:07 +0200

    theres no point

--- build_files/build.sh
+++ build_files/build.sh
@@ -12,7 +12,7 @@ dnf5 install -y sway-config-fedora swayfx NetworkManager-openvpn golang \
   nm-connection-editor network-manager-applet pavucontrol wlogout \
   blueman-applet btop ddcutil light qt6ct nwg-look alacritty ripgrep \
   gammastep gvfs nautilus SwayNotificationCenter chezmoi make gcc cliphist \
-  java-21-openjdk maven grim slurp yazi age gtklock ImageMagick greetd \
+  java-25-openjdk maven grim slurp yazi age gtklock ImageMagick greetd \
   gtkgreet python3-tmuxp
 
 dnf5 copr disable -y lihaohong/yazi

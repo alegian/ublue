58c741ecb617f2fc39924e096f5f7d6e9b645476
alegian <alegiannx@gmail.com>
2026-09-04 17:29:38 +0300

    remove duplicate

--- build_files/build.sh
+++ build_files/build.sh
@@ -46,8 +46,7 @@ dnf5 install -y \
     /akmods/kmods/*openrazer*.rpm \
     /akmods/common/*openrazer*.rpm
 
-dnf5 config-manager addrepo --from-repofile=https://openrazer.github.io/hardware:razer.repo
-dnf5 install -y openrazer-daemon polychromatic
+dnf5 install -y polychromatic
 
 bash "$(dirname "$0")/neovim.sh"
 
